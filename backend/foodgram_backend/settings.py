d9332b9c06ae250fa9633c032923cd2e247e70f6
Hengich <daniil.esakov@mail.ru>
2024-11-11 23:00:25 +0300

    Fix RecipeSerializer

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'http://127.0.0.1:8000').split()
 CSRF_TRUSTED_ORIGINS = [os.getenv('TRUST_URL', 'http://127.0.0.1:8000')]
